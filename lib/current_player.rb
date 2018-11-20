f53da0d2a0e2e1b81087e0fc6208141cf3ccc4b7
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-20 13:10:44 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 def turn_count(board)
   board.each do |index|
     count = 0
-    if board[index] == "O" || board[index] == "X"
+    if index == "O" || index == "X"
       count += 1
     end
     return count
